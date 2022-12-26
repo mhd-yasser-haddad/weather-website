0143c9a65d827994f614dde3ab7fb4022f11f4f7
Yasser Haddad <yasserhaddad99@gmail.com>
2022-12-26 19:33:04 +0200

    Remove message from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side Js');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-one');
