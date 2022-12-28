34eb96a8076397aed30414113965214057dcf627
MHD Yasser Haddad <yasserhaddad99@gmail.com>
2022-12-28 11:29:59 +0200

    Setup for heroku
    Edits to setup the app for Heroku that I'll never use

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ const messageTow = document.querySelector('#message-two');
 
 
 const fetchWeather = (address = "!") => {
-    fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+    fetch('/weather?address=' + address).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
